251d0c46cc076c56087cbb563a4d2afc50e7a6f6
adeelkbw <adeelnasirkbw@gmail.com>
2022-03-07 23:26:24 +0500

    Server setup connection with mongodb

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require('cors');
+const morgan = require('morgan')
+const fs = require('fs')
+const connectDB = require('./dotenvVariables/db')
+
+dotenv.config({ path: "./dotenvVariables/config.env" })
+
+const app = express()
+const PORT = process.env.PORT || 5000
+connectDB()
+
+
+if (process.env.NODE_ENV = "development") {
+    // app.use(morgan) //use as middleware log your requests in console
+    app.use(morgan('dev')); //
+}
+app.use(cors()) //will able to run server and client on diffrenet doamin or ports
+
+
+
+
+
+
+const server = app.listen(PORT, console.log(`Backend is listening ${process.env.NODE_ENV} at port ${PORT}`.brightCyan.bold))
+//unhandleRejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`ERROR : ${err.message}`);
+    server.close(() => process.exit(1))
+})
