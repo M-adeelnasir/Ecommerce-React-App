acc2b91ca401a5fda22d3deeae8e6add2e7cecc9
adeelkbw <adeelnasirkbw@gmail.com>
2022-03-27 23:17:11 +0500

    React Image Modal in Cart page

--- client/src/components/products/TableItemsCard.js
+++ client/src/components/products/TableItemsCard.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import ModalImage from "react-modal-image";
+
 
 const TableItemsCard = ({ p }) => {
     return (
         <tbody>
             <tr>
-                <td>Image</td>
+                <td>
+                    <div style={{ width: "100px", hight: "auto" }}> {p.images.length ? (<ModalImage small={p.images[1].url} large={p.images[1].url} />) : ("No Image")} </div>
+                </td>
                 <td>{p.title}</td>
                 <td>{p.price}</td>
                 <td>{p.brand}</td>
